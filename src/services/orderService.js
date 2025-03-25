76f50ab1916d136c8d9947b68938fb5af5b91f78
KhanhDeddo <khanhdeddo@gmail.com>
2025-03-25 18:38:43 +0700

    updatebykhanhdeddo

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,14 +1,45 @@
-import { Order, OrderItem } from '~/models/relations'
+import { Book, Order, OrderItem } from '~/models/relations'
 
 const getOrder = async (req) => {
-  const { user_id } = req.query
-  let whereCondition = user_id?{ user_id }:{}
-  const hehe = await Order.findAll(
-    { include: [{ model: OrderItem }] },
-    { where:whereCondition }
-  )
-  return hehe
+  const { user_id, order_id } = req.query
+  let whereCondition = {}
+  if (user_id) whereCondition.user_id = user_id
+  if (order_id) {
+    whereCondition.order_id = order_id
+    const order = await Order.findOne({
+      where: whereCondition,
+      include: [
+        {
+          model: OrderItem,
+          include: [
+            {
+              model: Book,
+              attributes: ['title', 'image_url', 'price']
+            }
+          ]
+        }
+      ]
+    })
+    return order
+  }
+
+  const orders = await Order.findAll({
+    where: whereCondition,
+    include: [
+      {
+        model: OrderItem,
+        include: [
+          {
+            model: Book,
+            attributes: ['title', 'image_url']
+          }
+        ]
+      }
+    ]
+  })
+  return orders
 }
+
 const createOrder = async (reqBody) => {
   const { user_id, name, phone, address, delivery_infor_id, customer_infor_id, status, total_price, discount_applied, final_price } = reqBody
   const newOrder = await Order.create({
@@ -26,7 +57,7 @@ const updateOrder = async (req) => {
 const deleteOrder = async (reqBody) => {
   const { order_id } = reqBody
   const delOrder = Order.destroy({
-    where:{order_id}
+    where: { order_id }
   })
   return delOrder
 }
